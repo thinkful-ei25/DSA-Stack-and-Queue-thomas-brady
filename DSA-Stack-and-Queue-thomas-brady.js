9f944d5f64f40c840355b10ef3ec542e446f0844
ThomasChun <tchun2055@gmail.com>
2018-12-14 06:28:00 -1000

    useful methods for stack

--- DSA-Stack-and-Queue-thomas-brady.js
+++ DSA-Stack-and-Queue-thomas-brady.js
@@ -1,1 +1,59 @@
 'use strict';
+
+class _Node {
+  constructor(data, next) {
+    this.next = next;
+    this.data = data;
+  }
+}
+
+class Stack {
+  constructor() {
+    this.top = null;
+  }
+
+  push(data) {
+    if (this.top === null) {
+      this.top = new _Node(data, null);
+      return this.top;
+    }
+
+    const node = new _Node(data, this.top);
+    this.top = node;
+  }
+
+  pop() {
+    const node = this.top;
+    this.top = node.next;
+    return node.data;
+  }
+}
+
+function peek(stack) {
+  console.log(stack.top.data);
+}
+
+function display(stack) {
+  let currentNode = stack.top;
+  while (currentNode !== null) {
+    console.log(currentNode.data);
+    currentNode = currentNode.next;
+  }
+}
+
+function main() {
+  let starTrek = new Stack();
+  starTrek.push('Kirk');
+  peek(starTrek);
+  starTrek.push('Spock');
+  peek(starTrek);
+  starTrek.push('McCoy');
+  peek(starTrek);
+  starTrek.push('Scotty');
+  peek(starTrek);
+  display(starTrek);
+  starTrek.pop();
+  display(starTrek);
+}
+
+main();
